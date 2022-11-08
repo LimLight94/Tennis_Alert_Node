b4a831c8fbc236e34399a300f60dc961a986b72f
limchaemyung <limchaemyung@lgcns.com>
2022-11-08 17:15:44 +0900

    feat: add timestamps

--- src/tennis.model.ts
+++ src/tennis.model.ts
@@ -3,13 +3,13 @@ import {Schema, model} from "mongoose";
 const dateAndTimeSchema: Schema = new Schema<IDateAndTime>({
     date: {type: String, required: true},
     time: {type: [String], required: false},
-});
+}, {id: false});
 
 const tennisSchema: Schema = new Schema<ITennis>({
     name: {type: String, required: true, unique: true},
     orgLink: {type: String, required: false},
     dateAndTime: [dateAndTimeSchema]
-});
+}, {versionKey: false, timestamps: true});
 
 export interface ITennis {
     name: string,

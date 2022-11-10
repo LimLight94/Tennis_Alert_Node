83fdea67f5b10ea5cdeabddaa0024136a55a2398
limchaemyung <limchaemyung@lgcns.com>
2022-11-11 04:05:31 +0900

    feat: slack api 추가

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,49 @@
 import express, {Request, Response} from "express";
 import {Tennis, DateAndTime, ITennis} from "./tennis.model"
 import connect from "./db"
+import {sendBotMsg} from "./slack";
 
 const app = express()
 const port = process.env.port || 3000
 const dbAddress = "mongodb://localhost:27017/tennis"
+const cors = require('cors');
+let corsOptions = {
+    origin: "*", // 출처 허용 옵션
+    credential: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+};
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
 });
 
+app.get("/alert", (req: Request, res: Response) => {
+    const baseUrl = `${req.protocol}://${req.hostname}:${port}`
+    Tennis.find().then(async (courts: any) => {
+        await sendBotMsg(baseUrl, courts)
+        res.status(200).send("OK")
+    }).catch((err: any) => res.status(500).send(err));
+});
+
 app.get("/courts", (req: Request, res: Response) => {
     Tennis.find()
-        .then((tennis:any) => {
+        .then((tennis: any) => {
             if (!tennis.length) return res.status(404).send({err: 'Court not found'});
             res.send(tennis);
         })
-        .catch((err:any) => res.status(500).send(err));
+        .catch((err: any) => res.status(500).send(err));
 });
 
 app.post("/courts", (req: Request, res: Response) => {
     let reqBody: ITennis = req.body
     Tennis.findOneAndUpdate({name: reqBody.name}, reqBody, {new: true, upsert: true})
         .then(
-            (tennis:any) => res.send(tennis)
-        ).catch((err:any) => res.status(500).send(err));
+            (tennis: any) => res.send(tennis)
+        ).catch((err: any) => res.status(500).send(err));
 });
 
 app.listen(port, () => {
---
+++ src/slack.ts
@@ -0,0 +1,66 @@
+const {WebClient, LogLevel} = require("@slack/web-api");
+
+const SLACK_API_TOKEN = "xoxb-4366820225681-4366847516641-WRqUZEY1hVyNBoXC6IhulP5Z";
+const client = new WebClient(SLACK_API_TOKEN, {
+    logLevel: LogLevel.DEBUG
+});
+
+const msgBlock: any[] = [];
+
+const makeLinkButton = (link: string) => {
+    console.log("test" + link)
+    return {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "<" + link + "|:Tennis:전체목록 보러가기>"
+        }
+    }
+}
+
+const makeCourtNameSection = (courtName: string) => {
+    return {
+        "type": "section",
+        "text": {
+            "type": "plain_text",
+            "text": courtName,
+            "emoji": true
+        }
+    }
+}
+
+const makeCourtDateAndTime = (dateAndTime: Array<any>) => {
+    let result = dateAndTime.map(item => {
+        let result = `${item.date}\n${item.time}`
+        return {
+            "type": "section",
+            "text": {
+                "type": "plain_text",
+                "text": result,
+                "emoji": true
+            }
+        }
+    })
+    let divider: any = {"type": "divider"}
+    result.push(divider)
+    return result
+}
+
+
+export const sendBotMsg = async (baseUrl: string, courts: any) => {
+    try {
+        msgBlock.push(makeLinkButton(baseUrl))
+        courts.forEach((item: any, index: number) => {
+            msgBlock.push(makeCourtNameSection(item.name))
+            msgBlock.push(...makeCourtDateAndTime(item.dateAndTime))
+        })
+        const result = await client.chat.postMessage({
+            channel: "#alert",
+            blocks: msgBlock
+        });
+
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
